3c91011301cd468f816ed936d113fb9c5e45f6f5
Pedro H. Oliveira <pedroholiveira@alu.ufc.br>
2025-02-04 00:20:27 -0300

    constantes compartilhadas (cores, estilos)

---
+++ J_Ratinho/constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    white: "#FFFFFF",
+    blue: "#9DB1F3",
+    green: "#AEA434",
+    yellow: "#FFCF3B",
+};
---
+++ J_Ratinho/constants/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    h1: {
+        fontSize: 32    
+    },
+    h2: {
+        fontSize: 24
+    },
+    label: {
+        fontSize: 16
+    }
+});
