a177ec83421126e94c9947781eb569234d9bf3a2
Pedro H. Oliveira <pedroholiveira@alu.ufc.br>
2025-02-04 12:47:40 -0300

    atualizaçao constantes

--- J_Ratinho/constants/styles.ts
+++ J_Ratinho/constants/styles.ts
@@ -1,8 +1,10 @@
 import {StyleSheet} from "react-native";
+import {colors} from "@/constants/colors";
 
 export const styles = StyleSheet.create({
     h1: {
-        fontSize: 32    
+        fontSize: 32,
+        fontWeight: "bold",
     },
     h2: {
         fontSize: 24
@@ -12,5 +14,17 @@ export const styles = StyleSheet.create({
     },
     label: {
         fontSize: 16
+    },
+    marca: {
+        width: 128
+    },
+    input: {
+        height: 32,
+        width: 256,
+        margin: 8,
+        borderWidth: 2,
+        borderRadius: 16,
+        padding: 10,
+        borderColor: colors.dark.blue
     },
 });
