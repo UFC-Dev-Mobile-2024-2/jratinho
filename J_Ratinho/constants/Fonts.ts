ac6ef923fc99d068123f695e1988f9fda304034d
andissudd <andissu.dd@gmail.com>
2025-02-04 13:04:48 -0300

    style(fonts): Adicionando fontes

--- J_Ratinho/constants/Fonts.ts
+++ J_Ratinho/constants/Fonts.ts
@@ -1,10 +1,13 @@
 import { useFonts } from 'expo-font';
+
 import {   DMSans_400Regular,
     DMSans_400Regular_Italic,
     DMSans_500Medium,
     DMSans_500Medium_Italic,
     DMSans_700Bold,
     DMSans_700Bold_Italic } from '@expo-google-fonts/dm-sans';
+
+import {   Capriola_400Regular } from '@expo-google-fonts/capriola';
 
 export const useCustomFonts = () => {
   const [fontsLoaded] = useFonts({
@@ -14,6 +17,7 @@ export const useCustomFonts = () => {
     DMSans_500Medium_Italic,
     DMSans_700Bold,
     DMSans_700Bold_Italic,
+    Capriola_400Regular,
   });
 
   return fontsLoaded;
