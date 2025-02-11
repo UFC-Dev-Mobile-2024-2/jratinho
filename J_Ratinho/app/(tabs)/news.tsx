8a75bbb6b946551b11915dbb61ef94e6068d8429
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2025-02-11 17:50:16 -0300

    upando o topbard de news

--- J_Ratinho/app/(tabs)/news.tsx
+++ J_Ratinho/app/(tabs)/news.tsx
@@ -3,13 +3,14 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import NewsCard from '../../components/NewsCard';
 import CardList from '../../components/CardList'
 import { PaperProvider } from "react-native-paper";
+import  TopBarNews from "../../components/TopBarNews";
 
 const News = () => {
     return (
         <PaperProvider>
             <SafeAreaProvider>
                 <SafeAreaView style={{flex:1}}>
-                    <h1>Noticias</h1>
+                    <TopBarNews />
                     <CardList />
                 </SafeAreaView>
             </SafeAreaProvider>
