76878ed8f3a0fbc039e047b968bf27b2cb07404d
Sabrina-Dias <sabrinadsd@alu.ufc.br>
2025-02-17 21:37:43 -0300

    feature sobre

--- J_Ratinho/app/(tabs)/index.tsx
+++ J_Ratinho/app/(tabs)/index.tsx
@@ -1,49 +1,52 @@
-import React from 'react';
-import {StyleSheet, View, Text, Platform} from "react-native";
-import {Link} from "expo-router";
-import {Button, FAB} from "react-native-paper";
+import React from "react";
+import { StyleSheet, View, Text, Platform } from "react-native";
+import { Link } from "expo-router";
+import { Button, FAB } from "react-native-paper";
 
-import ExerciceCard from '@/components/exercises/ExerciseCard';
-import Instrument from '@/components/instruments/Instrument';
-import InstrumentButton from '@/components/exercises/InstrumentButton';
-import QuestionOption from '@/components/exercises/QuestionOption';
+import ExerciceCard from "@/components/exercises/ExerciseCard";
+import Instrument from "@/components/instruments/Instrument";
+import InstrumentButton from "@/components/exercises/InstrumentButton";
+import QuestionOption from "@/components/exercises/QuestionOption";
+import About from "@/pages/About";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-        {/* <InstrumentButton/> */}
+  return (
+    <View style={styles.container}>
+      {/* <InstrumentButton /> */}
 
-        {/* <ExerciceCard>
+      {/* <ExerciceCard>
         </ExerciceCard> */}
-        
-        {/* <FAB icon="close"
+      {/* 
+      <FAB
+        icon="close"
         style={styles.fab}
-        onPress={() => console.log('Pressed')}
-        /> */}
+        onPress={() => console.log("Pressed")}
+      />
 
-        {/* <Instrument InstrumentVariant='tuba' InstrumentSize = {200} ></Instrument>
-        <Instrument InstrumentVariant='saxofone' InstrumentSize = {200} ></Instrument> */}
+      <Instrument InstrumentVariant="tuba" InstrumentSize={200}></Instrument>
+      <Instrument
+        InstrumentVariant="saxofone"
+        InstrumentSize={200}
+      ></Instrument> */}
 
-        <QuestionOption OptionState=""/>
-        <QuestionOption OptionState="wrong"/>
-        <QuestionOption OptionState="right"/>
-        
-        
+      {/* <QuestionOption OptionState="" />
+      <QuestionOption OptionState="wrong" />
+      <QuestionOption OptionState="right" /> */}
+      <About />
     </View>
-    );
+  );
 }
 
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          justifyContent: 'center',
-          backgroundColor: '#ecf0f1',
-          padding: 8,
-        },
-        fab: {
-    position: 'absolute',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  fab: {
+    position: "absolute",
     margin: 16,
     right: 0,
     bottom: 0,
   },
-      });
+});
