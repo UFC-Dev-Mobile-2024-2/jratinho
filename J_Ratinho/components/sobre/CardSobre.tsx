4ca871ffd9e48b7530cfe42f43935c9455cf0d45
letsmacmel <letsmaciel@alu.ufc.br>
2025-02-18 09:21:56 -0300

    feat(Home): Nova Home

--- J_Ratinho/components/sobre/CardSobre.tsx
+++ J_Ratinho/components/sobre/CardSobre.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import { Avatar, Card, IconButton, Title, Paragraph } from 'react-native-paper';
-
 
   type CardSobreProps={ 
     cardTitle:string;
@@ -9,22 +8,45 @@ import { Avatar, Card, IconButton, Title, Paragraph } from 'react-native-paper';
 
   }
 
-
-
-  const CardSobre = (props:CardSobreProps
-
-  ) => {
+  const CardSobre = (props:CardSobreProps)=> {
     return (
       <Card style={{ margin: 10 }}>
-        <Card.Content style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Avatar.Icon size={50} icon="account" />
-          <View style={{ marginLeft: 10, flex: 1 }}>
+        <Card.Content>
+        <View style={styles.content}>
+        <Image
+            source={{ uri: "https://stories.bnews.com.br/motivos-para-o-vira-lata-caramelo-se-tornar-patrimonio-nacional-brasileiro/assets/21.jpeg" }}
+            style={styles.image}
+            resizeMode="contain"
+          />
+            
+            <View style={styles.textContainer}>
             <Title>{props.cardTitle}</Title>
             <Paragraph>{props.cardParagraph}</Paragraph>
+            </View>
           </View>
         </Card.Content>
       </Card>
     );
   };
+
+
+  const styles = StyleSheet.create({
+    card: {
+      margin: 10,
+    },
+    textContainer: {
+      flex: 1,
+    },
+    content: {
+      alignSelf: "flex-start",
+    },
+
+    image: {
+      width: 10, 
+      marginTop: 10,
+      marginLeft: 10, 
+      resizeMode: "cover",
+    },
+  });
 
 export default CardSobre;
