1ba95072fd09ba020606facf17787093ca3ed1d6
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-05 05:21:33 -0300

    style: formata página de autenticação

--- J_Ratinho/app/_layout.tsx
+++ J_Ratinho/app/_layout.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Slot, Stack } from "expo-router";
 import { useColorScheme, View } from "react-native";
 import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  PaperProvider,
-  Appbar,
+	MD3DarkTheme,
+	MD3LightTheme,
+	PaperProvider,
+	Appbar,
 } from "react-native-paper";
 import { Colors } from "@/constants/Colors";
 
@@ -13,17 +13,18 @@ const dark = { ...MD3DarkTheme, colors: Colors.dark };
 const light = { ...MD3LightTheme, colors: Colors.light };
 
 export default function RootLayout() {
-  return (
-    <PaperProvider theme={light}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="signin" options={{ headerShown: false }} />
+	return (
+		<PaperProvider theme={light}>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+				<Stack.Screen name="login" options={{ headerShown: false }} />
+				<Stack.Screen name="signin" options={{ headerShown: false }} />
+				<Stack.Screen name="auth" options={{ headerShown: false }} />
 
-        <Stack.Screen name="profile" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </PaperProvider>
-  );
+				<Stack.Screen name="profile" />
+				<Stack.Screen name="+not-found" />
+			</Stack>
+		</PaperProvider>
+	);
 }
---
+++ J_Ratinho/app/auth.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, KeyboardAvoidingView, Platform, Image } from "react-native";
+import { Text, TextInput } from "react-native-paper";
+import { useRouter } from "expo-router";
+
+import { Colors } from "@/constants/Colors";
+import { styles } from '@/constants/styles';
+
+export default function Login() {
+	const router = useRouter();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	return (
+		<KeyboardAvoidingView
+			style={local.container}
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+		>
+			<View style={local.header}>
+				<Image style={styles.marca} source={require("@/assets/images/marca.png")} resizeMode="contain" />
+			</View>
+			<Text style={{ width: "80 %", marginBottom: 32, color: '#9DB1F3', textAlign: "center", }} variant="headlineLarge">Está quase lá, acessa sua conta</Text>
+
+			<View style={{ width: "80%" }}>
+				<TextInput
+					label="E-mail"
+					mode="outlined"
+					outlineColor="#9DB1F3"
+					activeOutlineColor="#9DB1F3"
+					theme={{ colors: { primary: '#9DB1F3', onSurfaceVariant: '#9DB1F3' } }}
+					style={local.input}
+					onChangeText={setEmail}
+					value={email}
+				/>
+				<TextInput
+					label="Senha"
+					mode="outlined"
+					outlineColor="#9DB1F3"
+					activeOutlineColor="#9DB1F3"
+					theme={{ colors: { primary: '#9DB1F3', onSurfaceVariant: '#9DB1F3' } }}
+					style={local.input}
+					onChangeText={setPassword}
+					value={password}
+					secureTextEntry
+				/>
+			</View>
+
+			<View style={{ width: "80%", alignItems: "flex-end" }}>
+				<TouchableOpacity onPress={() => router.push("/")}>
+					<Text style={{ color: "#9DB1F3", fontWeight: "bold" }}>Esqueci minha senha</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={local.buttonContainer}>
+				<TouchableOpacity style={local.button} onPress={() => router.push("/home")}>
+					<Text style={local.buttonText}>Entrar</Text>
+				</TouchableOpacity>
+
+			</View>
+		</KeyboardAvoidingView >
+	)
+}
+
+const local = StyleSheet.create({
+	header: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		position: "relative",
+	},
+	scrollViewContent: {
+		flexGrow: 1,
+		justifyContent: "center",
+		paddingBottom: 20,
+	},
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+	input: {
+		width: "100%",
+		backgroundColor: "transparent",
+		marginBottom: 10,
+		fontSize: 18,
+	},
+	buttonContainer: {
+		flex: 1,
+		justifyContent: "flex-end",
+		alignItems: "center",
+		width: "60%",
+		paddingBottom: 40,
+	},
+	button: {
+		width: "100%",
+		height: 50,
+		backgroundColor: Colors.dark.green,
+		borderRadius: 25,
+		justifyContent: "center",
+		alignItems: "center",
+		marginBottom: 16,
+	},
+	buttonText: {
+		fontSize: 18,
+		color: Colors.dark.text,
+		fontWeight: "bold",
+	}
+});
