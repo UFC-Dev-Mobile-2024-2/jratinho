4d92144ec0e837ae85a57d117a7205174d4c161a
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-05 05:17:16 -0300

    style: formata página de login

--- J_Ratinho/app/login.tsx
+++ J_Ratinho/app/login.tsx
@@ -1,77 +1,112 @@
-import React from "react";
-import {StyleSheet, View, Image, TouchableOpacity} from "react-native";
-import {Text, TextInput, Button} from "react-native-paper";
-import {Link} from "expo-router";
-import {useRouter} from "expo-router";
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, KeyboardAvoidingView, Platform, Image } from "react-native";
+import { Text, TextInput } from "react-native-paper";
+import { useRouter } from "expo-router";
 
-import {styles} from "@/constants/styles";
 import { Colors } from "@/constants/Colors";
+import { styles } from '@/constants/styles';
 
 export default function Login() {
-    const router = useRouter();
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+	const router = useRouter();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    return (
-        <View style={local.container}>
-            <Image style={styles.marca} source={require("@/assets/images/marca.png")} resizeMode="contain"/>
-            <Text style={local.title}>Bem vindo de volta!</Text>
-            
-            <View style={{width: "80%"}}>
-                <TextInput
-                    style={local.input}
-                    label="E-mail"
-                    mode="outlined"
-                    onChangeText={setEmail}
-                    value={email}
-                />
-                <TextInput
-                    style={local.input}
-                    label="Senha"
-                    mode="outlined"
-                    onChangeText={setPassword}
-                    value={password}
-                />
-            </View>
+	return (
+		<KeyboardAvoidingView
+			style={local.container}
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+		>
+			<View style={local.header}>
+				<Image style={styles.marca} source={require("@/assets/images/marca.png")} resizeMode="contain" />
+			</View>
+			<Text style={{ marginBottom: 32, color: '#9DB1F3' }} variant="headlineLarge">Bem-vindo de volta!</Text>
 
-            <TouchableOpacity onPress={() => router.push("/esqueci-senha")}>
-                <Text style={local.link}>Esqueci minha senha</Text>
-            </TouchableOpacity>
+			<View style={{ width: "80%" }}>
+				<TextInput
+					label="E-mail"
+					mode="outlined"
+					outlineColor="#9DB1F3"
+					activeOutlineColor="#9DB1F3"
+					theme={{ colors: { primary: '#9DB1F3', onSurfaceVariant: '#9DB1F3' } }}
+					style={local.input}
+					onChangeText={setEmail}
+					value={email}
+				/>
+				<TextInput
+					label="Senha"
+					mode="outlined"
+					outlineColor="#9DB1F3"
+					activeOutlineColor="#9DB1F3"
+					theme={{ colors: { primary: '#9DB1F3', onSurfaceVariant: '#9DB1F3' } }}
+					style={local.input}
+					onChangeText={setPassword}
+					value={password}
+					secureTextEntry
+				/>
+			</View>
 
-            <Button style={local.button} mode="contained" onPress={() => router.push("/")}> 
-                Entrar 
-            </Button>
+			<View style={{ width: "80%", alignItems: "flex-end" }}>
+				<TouchableOpacity onPress={() => router.push("/")}>
+					<Text style={{ color: "#9DB1F3", fontWeight: "bold" }}>Esqueci minha senha</Text>
+				</TouchableOpacity>
+			</View>
 
-            <TouchableOpacity onPress={() => router.push("/cadastrar")}>
-                <Text style={local.link}>Não tem uma conta? Cadastre-se</Text>
-            </TouchableOpacity>
+			<View style={local.buttonContainer}>
+				<TouchableOpacity style={local.button} onPress={() => router.push("/home")}>
+					<Text style={local.buttonText}>Entrar</Text>
+				</TouchableOpacity>
 
-        </View>
-    )
+				<TouchableOpacity onPress={() => router.push("/signin")}>
+					<Text>Não tem uma conta?
+						<Text style={{ color: '#AEA434', fontWeight: "bold" }} onPress={() => router.push("/signin")}> Cadastre-se</Text>
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</KeyboardAvoidingView >
+	)
 }
 
 const local = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "regular",
-        color: "#9DB1F3",
-        marginBottom: 20,
-    },
-    h1: {
-        color: Colors.light.cian,
-        marginBottom: 32,
-    },
-    input: {
-        flex: 1,
-        fontSize: 16,
-        color: "#3333"
-    },
-    button: {
-        minWidth: 256,
-        marginBottom: 8
-    }
+	header: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		position: "relative",
+	},
+	scrollViewContent: {
+		flexGrow: 1,
+		justifyContent: "center",
+		paddingBottom: 20,
+	},
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+	input: {
+		width: "100%",
+		backgroundColor: "transparent",
+		marginBottom: 10,
+		fontSize: 18,
+	},
+	buttonContainer: {
+		flex: 1,
+		justifyContent: "flex-end",
+		alignItems: "center",
+		width: "60%",
+		paddingBottom: 40,
+	},
+	button: {
+		width: "100%",
+		height: 50,
+		backgroundColor: Colors.dark.green,
+		borderRadius: 25,
+		justifyContent: "center",
+		alignItems: "center",
+		marginBottom: 16,
+	},
+	buttonText: {
+		fontSize: 18,
+		color: Colors.dark.text,
+		fontWeight: "bold",
+	}
 });
