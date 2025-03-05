a33b521cbe8c7affe5a9790511c20289e30a44f4
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-05 05:19:40 -0300

    fix: adiciona index e home

---
+++ J_Ratinho/app/(tabs)/home.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, View, Image } from "react-native";
+import { Text, Button, Card, Avatar } from "react-native-paper";
+import { Link } from "expo-router";
+import { styles } from "@/constants/styles";
+import { Colors } from "@/constants/Colors";
+
+import ExerciceCard from '@/components/exercises/ExerciseCard';
+
+export default function Index() {
+	return (
+		<View style={local.container}>
+			<View style={local.header}>
+				<Image style={styles.marca} source={require("@/assets/images/marca.png")} resizeMode="contain" />
+				<Link style={local.profile} href="/profile">
+					<Avatar.Image size={40} source={require("@/assets/images/rato.png")} />
+				</Link>
+			</View>
+			<Text variant="headlineLarge">Bem vindo, aluno!</Text>
+			<Text style={{ marginBottom: 32 }} variant="titleMedium">Vamos aprender e se divertir?</Text>
+
+			<ExerciceCard InstrumentVariant='saxofone' InstrumentSize={200} InstrumentRotation='-30deg' MainColor={Colors.dark.orange} CoverColor={Colors.light.orange} TextColor='#ffffff'>
+			</ExerciceCard>
+
+			<ExerciceCard InstrumentVariant='saxofone' InstrumentSize={200} InstrumentRotation='-30deg' MainColor={Colors.dark.orange} CoverColor={Colors.light.orange} TextColor='#ffffff'>
+			</ExerciceCard>
+
+
+
+		</View>
+	)
+}
+
+const local = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingLeft: 16,
+		paddingRight: 16,
+		alignItems: "center",
+	},
+	header: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		position: "relative",
+	},
+	card: {
+		marginBottom: 8,
+		width: "100%"
+	},
+	profile: {
+		position: "absolute",
+		right: -128
+	}
+});
